fd45dcf0b6dca509f17c5cb8b528460b90241ce4
Mark Mansi <markm@cs.wisc.edu>
2019-08-02 12:50:59 -0500

    use 9s for time_loop

--- plot-time-elapsed-deriv-cycles-cdf.py
+++ plot-time-elapsed-deriv-cycles-cdf.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python2
 
 import matplotlib.pyplot as plt
+from matplotlib import scale as mscale
+from matplotlib import transforms as mtransforms
+from matplotlib.ticker import FixedFormatter, FixedLocator
+
 import numpy as np
+from numpy import ma
 
 import re
 import itertools
@@ -44,6 +49,63 @@ for arg in argv[1:]:
                 except ValueError:
                     print("bad float: %s" % line)
 
+# Source: https://stackoverflow.com/questions/31147893/logarithmic-plot-of-a-cumulative-distribution-function-in-matplotlib
+class CloseToOne(mscale.ScaleBase):
+    name = 'close_to_one'
+
+    def __init__(self, axis, **kwargs):
+        mscale.ScaleBase.__init__(self)
+        self.nines = kwargs.get('nines', 5)
+
+    def get_transform(self):
+        return self.Transform(self.nines)
+
+    def set_default_locators_and_formatters(self, axis):
+        axis.set_major_locator(FixedLocator(
+                np.array([(1-10**(-k)) * 100.0 for k in range(1+self.nines)])))
+        axis.set_major_formatter(FixedFormatter(
+                [str((1-10**(-k))*100.0) for k in range(1+self.nines)]))
+
+
+    def limit_range_for_scale(self, vmin, vmax, minpos):
+        return vmin, min((1 - 10**(-self.nines))*100.0, vmax)
+
+    class Transform(mtransforms.Transform):
+        input_dims = 1
+        output_dims = 1
+        is_separable = True
+
+        def __init__(self, nines):
+            mtransforms.Transform.__init__(self)
+            self.nines = nines
+
+        def transform_non_affine(self, a):
+            masked = ma.masked_where(a > (1-10**(-1-self.nines))*100.0, a)
+            if masked.mask.any():
+                return -ma.log10(100.0-a)
+            else:
+                return -np.log10(100.0-a)
+
+        def inverted(self):
+            return CloseToOne.InvertedTransform(self.nines)
+
+    class InvertedTransform(mtransforms.Transform):
+        input_dims = 1
+        output_dims = 1
+        is_separable = True
+
+        def __init__(self, nines):
+            mtransforms.Transform.__init__(self)
+            self.nines = nines
+
+        def transform_non_affine(self, a):
+            return (1. - 10**(-a)) * 100.0
+
+        def inverted(self):
+            return CloseToOne.Transform(self.nines)
+
+mscale.register_scale(CloseToOne)
+
 plt.figure(1, figsize=(5, 3.5))
 
 markers = itertools.cycle(MARKERS)
@@ -80,9 +142,10 @@ for i, (label, xs) in enumerate(data1.items()):
         h_plot, = plt.plot(cdfx, cdfy, label = "%s-nonlocal" % label, linestyle = '--', marker = 'None', color = colors.next())
         handles.append(h_plot)
 
-plt.ylim((0, 100))
+plt.ylim((0, 99.999))
 
 plt.xscale('log')
+plt.yscale('close_to_one')
 
 plt.ylabel("% of Measurements")
 plt.xlabel("$\Delta$ Time (cycles)")
