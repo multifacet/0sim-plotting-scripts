3aecc343cd8d62c396672321dc8ad61bccd44382
Mark Mansi <markm@cs.wisc.edu>
2019-11-26 10:19:06 -0600

    update paperstyle

--- paperstyle.py
+++ paperstyle.py
@@ -1,10 +1,26 @@
 
 import os
+import matplotlib
+import matplotlib.pyplot as plt
 
-COLORS=["r", "k", "b", "g"]
+COLORS=["r", "k", "b", "g", "y", "purple", "cyan"]
 MARKERS = ['.', '>', '<', '*', 'v', '^', 'D', 'X', 'P', 'p', 's']
 LINE_STYLES=[":","-.","--","-"]
 
 IS_PDF = True
 
-FIGSIZE=(3, 2.5) if os.environ.get("SMALL_PLOT") is not None else (5, 3.5)
+SLIDES_FONT_SIZE = 18
+
+def _figsize():
+    if os.environ.get("SMALL_PLOT") is not None:
+        return (3, 2.5)
+    elif os.environ.get("SLIDE_PLOT") is not None:
+        # Side-effect: increase font size, set title
+        matplotlib.rcParams.update({'font.size': SLIDES_FONT_SIZE})
+        plt.title(os.environ.get("SLIDE_PLOT"))
+
+        return (8, 6)
+    else:
+        return (5, 3.5)
+
+FIGSIZE=_figsize()
