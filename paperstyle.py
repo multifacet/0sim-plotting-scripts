59e06f2dbfb444c1cff018821a526337f954cfe2
Mark Mansi <markm@cs.wisc.edu>
2020-01-20 18:18:02 -0600

    no type 3 fonts

--- paperstyle.py
+++ paperstyle.py
@@ -23,4 +23,37 @@ def _figsize():
     else:
         return (5, 3.5)
 
+def SetPlotRC():
+    #If fonttype = 1 doesn't work with LaTeX, try fonttype 42.
+    plt.rc('pdf',fonttype = 42)
+    plt.rc('ps',fonttype = 42)
+
+def ApplyFont(ax):
+
+    ticks = ax.get_xticklabels() + ax.get_yticklabels()
+
+    text_size = 14.0
+
+    for t in ticks:
+        t.set_fontname('Times New Roman')
+        t.set_fontsize(text_size)
+
+    txt = ax.get_xlabel()
+    txt_obj = ax.set_xlabel(txt)
+    txt_obj.set_fontname('Times New Roman')
+    txt_obj.set_fontsize(text_size)
+
+    txt = ax.get_ylabel()
+    txt_obj = ax.set_ylabel(txt)
+    txt_obj.set_fontname('Times New Roman')
+    txt_obj.set_fontsize(text_size)
+
+    txt = ax.get_title()
+    txt_obj = ax.set_title(txt)
+    txt_obj.set_fontname('Times New Roman')
+    txt_obj.set_fontsize(text_size)
+
 FIGSIZE=_figsize()
+
+# Use type1 fonts
+SetPlotRC()
